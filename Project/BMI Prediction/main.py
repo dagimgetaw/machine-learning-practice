5a13c8878a860ef4e210e2ffe12b7e3a5e423b7e
Dagim <dagimgetaW27@gmail.com>
2025-01-31 10:35:11 +0300

    Train a Random Forest Classifier

--- Project/BMI Prediction/main.py
+++ Project/BMI Prediction/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 bmi_dataset = pd.read_csv("bmi.csv")
 bmi_dataset.drop_duplicates(inplace=True)
@@ -12,5 +13,10 @@ x = bmi_dataset.drop(columns=['Index'])
 y = bmi_dataset['Index']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(x_train, y_train)
+
+prediction = model.predict(x_test)
 
 print(bmi_dataset.head(10))
