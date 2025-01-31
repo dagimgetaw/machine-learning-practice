42d11c798701cd237e0e5530fa9b01695e4a235a
Dagim <dagimgetaW27@gmail.com>
2025-01-31 10:33:07 +0300

    split the dataset into training and testing

--- Project/BMI Prediction/main.py
+++ Project/BMI Prediction/main.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 bmi_dataset = pd.read_csv("bmi.csv")
 bmi_dataset.drop_duplicates(inplace=True)
 
 label_encoder = LabelEncoder()
 bmi_dataset['Gender'] = label_encoder.fit_transform(bmi_dataset['Gender'])
+
+x = bmi_dataset.drop(columns=['Index'])
+y = bmi_dataset['Index']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 print(bmi_dataset.head(10))
