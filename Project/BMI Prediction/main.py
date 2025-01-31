6a0c473da13303c2870d065a13570e3b2ff74f1a
Dagim <dagimgetaW27@gmail.com>
2025-01-31 10:27:39 +0300

    change the Gender row to num value

--- Project/BMI Prediction/main.py
+++ Project/BMI Prediction/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 bmi_dataset = pd.read_csv("bmi.csv")
 bmi_dataset.drop_duplicates(inplace=True)
+
+label_encoder = LabelEncoder()
+bmi_dataset['Gender'] = label_encoder.fit_transform(bmi_dataset['Gender'])
 
 print(bmi_dataset.head(10))
